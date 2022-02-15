da7e5f479113035c57f6c3d3be9357940aedaaa3
Devon <devon_3273@hotmail.com>
2022-02-15 11:40:02 -0700

    adding functionality

--- app.js
+++ app.js
@@ -10,6 +10,25 @@ window.addEventListener('load', () => {
 
     if (!task) {
       alert('Please Enter A Task.');
+      return;
     }
+
+    const task_el = document.createElement('div');
+    task_el.classList.add('task');
+
+    const task_content_el = document.createElement('div');
+    task_content_el.classList.add('content');
+
+    task_el.appendChild(task_content_el);
+
+    const task_input_el = document.createElement('input');
+    task_input_el.classList.add('text');
+    task_input_el.type = 'text';
+    task_input_el.value = task;
+    task_input_el.setAttribute('readonly', 'readonly');
+
+    task_content_el.appendChild(task_input_el);
+
+    list_el.appendChild(task_el);
   })
 })
